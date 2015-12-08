5d3294260d691c37e8ec209b19fe133484854421
Nicole Rauch <nicole.m@gmx.de>
2015-12-08 22:46:57 +0000

    Test cleanup.

--- test/UsernameTest.js
+++ test/UsernameTest.js
@@ -6,34 +6,29 @@ import TestUtils from "react/lib/ReactTestUtils";
 import expect from "must";
 import sinon from "sinon";
 
+import Username from "../src/Username.js";
+
 describe("UsernameTest", function () {
 
-    describe("onChange handler called when", function () {
+    beforeEach(function () {
+        global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
+        global.window = global.document.defaultView;
 
-        let Username;
-
-        beforeEach(function () {
-            global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
-            global.window = global.document.defaultView;
-            Username = require("../src/Username.js");
-
-            this.validateInBackend = sinon.spy();
-            this.component = TestUtils.renderIntoDocument(<Username validateInBackend={this.validateInBackend}/>);
-        });
-
-        afterEach(function () {
-            delete global.document;
-            delete global.window;
-        });
-
-
-        it("input changes", function () {
-            var node = ReactDOM.findDOMNode(this.component.refs.username);
-            node.value = "myname";
-            TestUtils.Simulate.change(node);
-
-            expect(this.validateInBackend.firstCall.args).to.eql(["myname"]);
-        });
+        this.validateInBackend = sinon.spy();
+        this.component = TestUtils.renderIntoDocument(<Username validateInBackend={this.validateInBackend}/>);
     });
 
+    afterEach(function () {
+        delete global.document;
+        delete global.window;
+    });
+
+
+    it("onChange handler is called when input changes", function () {
+        var input = ReactDOM.findDOMNode(this.component.refs.username);
+        input.value = "myname";
+        TestUtils.Simulate.change(input);
+
+        expect(this.validateInBackend.firstCall.args).to.eql(["myname"]);
+    });
 });
